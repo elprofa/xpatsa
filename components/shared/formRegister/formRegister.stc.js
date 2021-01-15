36eb152d76b751491d1c79e4f8c7abadeca6b63a
elprofa wedo <wedoprofan14@gmail.com>
2021-01-15 14:45:16 +0100

    elprofa Traitement de la page inscription

---
+++ components/shared/formRegister/formRegister.stc.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { Container } from "reactstrap";
+
+ const FormRegisterStc = styled(Container)`
+
+    h1
+    {
+        margin-bottom:30px;
+        border-bottom: 1px solid #ecedee;
+    }
+    a
+    {
+        font-size:12px;
+    }
+    #left
+    {
+        text-align:left;
+    }
+    #right
+    {
+        text-align:right;
+    }
+`;
+
+export default FormRegisterStc;
--- pages/connexion.js
+++ pages/connexion.js
@@ -1,14 +1,31 @@
 import Link from 'next/link'
 import { Row,Col,Card} from "reactstrap";
+import styled from "styled-components";
 import FormLogin from "../components/shared/formLogin/formLogin";
+
+const Wrapper = styled(Row)`
+
+  background: #fbfbfb;
+  margin:0px;
+
+  .card{
+    width: 30rem;
+    margin:70px auto;
+    padding:20px;
+    border-color:#ecedee;
+    border-radius:0px;
+  }
+ 
+`;
+
 export default function Connexion() {
   return (
-      <Row style={{ background: '#fbfbfb',margin:'0px' }}>
+      <Wrapper>
         <Col>
-          <Card style={{ width: '30rem',margin:'70px auto',padding:'20px',borderColor:'#ecedee;' }}>
+          <Card>
             <FormLogin />
           </Card>
         </Col>
-      </Row>
+      </Wrapper>
   )
 }
