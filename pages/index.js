83a8e1bb6133861cf4e7c1ce9d942864ba357258
elprofa wedo <wedoprofan14@gmail.com>
2021-01-21 09:58:41 +0100

    elprofa merge avec la partie de kurt

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,11 @@ import Caroussel from "../components/shared/caroussel/caroussel";
 import { initializeApollo } from "../apollo/client";
 import ComponentBlockDevise from "../components/shared/ComponentBlockDevise/ComponentBlockDevise";
 import CommentCaFonctionne from "../components/shared/CommentCaFonctionne/CommentCaFonctionne";
+import GrilleComponentPays from '../components/shared/PaysDispo';
+import { Container, Button, Row, Col } from 'reactstrap';
+import FormContact from '../components/shared/formContact';
+import NewsLater from '../components/shared/Newlater';
+
 
 const ViewerQuery = gql `
   query ViewerQuery {
@@ -18,12 +23,13 @@ const ViewerQuery = gql `
   }
 `;
 
-const Index = () => {
+const Index = (props) => {
   const {
     data: { viewer },
   } = useQuery(ViewerQuery);
 
   const Wrapper = styled.section`
+    background:#ececec;
     img
     {
       width:100%;
@@ -63,6 +69,46 @@ const Index = () => {
 
       <ComponentBlockDevise />
       <CommentCaFonctionne />
+      <Container>
+          <Row>
+              <Col>
+                  <GrilleComponentPays/>
+              </Col>
+          </Row>
+      </Container>
+      <Wrapper >
+        <Container>
+            <Row>
+                <Col>
+                  <FormContact/>
+                </Col>
+            </Row>
+        </Container>
+      </Wrapper>
+      <Container className='d-flex justify-content-center'>
+        <Row className='w-100'>
+            <Col className='h-100 d-flex align-items-center justify-content-center'>
+
+                <NewsLater 
+                    position="right" 
+                    texteTitre="Newsletter"
+                    colorTitre="#007bff"
+                    paragraphe="Recevez votre newsletters en vous inscrivant ici." 
+                />
+            </Col>
+
+            <Col className='h-100 d-flex align-items-center justify-content-center'>
+                <Button 
+                    texte="S'inscrire "
+                    backgroundColor="#007bff"
+                    borderColor="007bff"
+                    color="#fff" />
+            </Col>
+        </Row>                  
+
+      </Container>
+      
+
     </div>
   );
 };
