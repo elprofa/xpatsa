af4e9defbfb7fb99bf5a1b4a3025a3a4e915e005
KurtMoucketou <kurtmoucketou@yahoo.fr>
2021-01-25 14:04:09 +0100

    Revert "StepFormOneOK"
    This reverts commit 92711631d51dda9becc2cb078be1573643fcb8d3.

--- pages/transfert.js
+++ pages/transfert.js
@@ -1,11 +1,10 @@
 import React from "react";
 import TransferForm from "../components/transferFrom";
-import FormStepOne from '../components/shared/EtapeOneForm';
 
 const TransfertPage = () => {
   return (
     <div>
-      <FormStepOne />
+      <TransferForm />
     </div>
   );
 };
