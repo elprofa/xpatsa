92711631d51dda9becc2cb078be1573643fcb8d3
KurtMoucketou <kurtmoucketou@yahoo.fr>
2021-01-25 13:59:09 +0100

    StepFormOneOK

--- pages/transfert.js
+++ pages/transfert.js
@@ -1,10 +1,11 @@
 import React from "react";
 import TransferForm from "../components/transferFrom";
+import FormStepOne from '../components/shared/EtapeOneForm';
 
 const TransfertPage = () => {
   return (
     <div>
-      <TransferForm />
+      <FormStepOne />
     </div>
   );
 };
