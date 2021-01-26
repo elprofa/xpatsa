8ece5d0723060d912803ad1cc64f6b1b49902209
David Mbadinga <34944852+DubaniWarrior@users.noreply.github.com>
2021-01-26 19:25:42 +0100

    updated height

--- pages/inscription.js
+++ pages/inscription.js
@@ -1,51 +1,49 @@
-import Link from 'next/link'
-import {Container, Row,Col,Card} from "reactstrap";
+import Link from "next/link";
+import { Container, Row, Col, Card } from "reactstrap";
 import styled from "styled-components";
 import FormRegister from "../components/shared/formRegister/formRegister";
-import Image from 'next/image';
-
+import Image from "next/image";
 
 const Wrapper = styled.div`
-
+  height: 75vh;
+  margin-top: 90px;
   background: #fbfbfb;
-  margin:0px;
+  margin: 0px;
 
-  #divImage div,#divImage img
-  {
-    width:100%; 
+  #divImage div,
+  #divImage img {
+    width: 100%;
   }
 
-  .card{
-    margin:70px auto;
-    padding:20px;
-    border-color:#ecedee;
-    border-radius:0px;
-    min-height:351px;
+  .card {
+    margin: 70px auto;
+    padding: 20px;
+    border-color: #ecedee;
+    border-radius: 0px;
+    min-height: 351px;
   }
 `;
 
 export default function Inscription() {
   return (
-      <Wrapper>
-        <Container className="h-100">
-            <Row className="h-100">
-                <Col lg={6} className="h-100 d-none d-lg-block" id="divImage">
-                    
-                    <Image 
-                          src='/img/computer.svg'
-                          alt="Picture of computer"
-                          width="100"
-                          height="100"
-                          />
-                    
-                </Col>
-                <Col lg={6} className="h-100 pr-0">
-                    <Card className="h-75">
-                        <FormRegister />
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-      </Wrapper>
-  )
+    <Wrapper>
+      <Container className="h-100">
+        <Row className="h-100">
+          <Col lg={6} className="h-100 d-none d-lg-block" id="divImage">
+            <Image
+              src="/img/computer.svg"
+              alt="Picture of computer"
+              width="100"
+              height="100"
+            />
+          </Col>
+          <Col lg={6} className="h-100 pr-0">
+            <Card className="h-75">
+              <FormRegister />
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </Wrapper>
+  );
 }
