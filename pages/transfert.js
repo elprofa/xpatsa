74402fdca48569c8e0cfddb579c1144ad22681cd
David Mbadinga <34944852+DubaniWarrior@users.noreply.github.com>
2021-01-26 12:13:09 +0100

    fixed transfer

--- pages/transfert.js
+++ pages/transfert.js
@@ -1,16 +1,11 @@
 import React from "react";
-import EtapeChoixPays from '../components/FormStepPays';
-
+import TransferForm from "../components/transferFrom";
 
 const TransfertPage = (props) => {
   return (
     <div>
-<<<<<<< HEAD
       <TransferForm />
       {console.log(props)}
-=======
-      <EtapeChoixPays />
->>>>>>> 11d4f098675692c95f0c050993421ced7c1a29c3
     </div>
   );
 };
