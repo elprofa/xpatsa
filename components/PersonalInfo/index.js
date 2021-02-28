d3764febf477c44ffa1f303eacf56623996f1e23
David Mbadinga <34944852+DubaniWarrior@users.noreply.github.com>
2021-02-28 18:41:40 +0100

    update titles

--- components/PersonalInfo/index.js
+++ components/PersonalInfo/index.js
@@ -2,25 +2,46 @@ import React from "react";
 import { Col, Row, Form, Input } from "reactstrap";
 //import InputGroupButtonDropdownStc from './FormPersonalInfo.stc';
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const Example = (props) => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [splitButtonOpen, setSplitButtonOpen] = useState(false);
 
-
   return (
     <Form>
-      <Row form >
-        <Col lg={12} className='d-flex flex-wrap justify-content-center mt-3 w-100'>
-        <Form className='w-100 mt-4'>
-      <Input type="select" name="select" id="exampleSelect" bsSize="lg" placeholder='Select Country'>
-          <option>Select Country</option>
-          <option>Gabon F.Cfa</option>
-          <option>Maroc Dirhams</option>
-          <option>Sud Afrique Rand</option>
-        </Input>
-    </Form>
+      <Row form>
+        <Col
+          lg={12}
+          className="d-flex flex-wrap justify-content-center mt-3 w-100"
+        >
+          <Form className="w-100 mt-4">
+            <Input
+              type="select"
+              name="select"
+              id="exampleSelect"
+              bsSize="lg"
+              placeholder="Select Country"
+            >
+              <option>Pays de l'expediteur</option>
+              <option>Gabon F.Cfa</option>
+              <option>Maroc Dirhams</option>
+              <option>Sud Afrique Rand</option>
+            </Input>
+            <Input
+              type="select"
+              name="select"
+              id="exampleSelect"
+              bsSize="lg"
+              placeholder="Select Country"
+              className="mt-3"
+            >
+              <option>Pays du destinataire</option>
+              <option>Gabon F.Cfa</option>
+              <option>Maroc Dirhams</option>
+              <option>Sud Afrique Rand</option>
+            </Input>
+          </Form>
         </Col>
       </Row>
     </Form>
