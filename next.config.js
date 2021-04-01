95fe3d5c531f71eb86ba278eaf18f301df3e6ac9
elprofa <76491497+elprofa@users.noreply.github.com>
2021-04-01 15:09:24 +0200

    commit 4 nprogress

--- next.config.js
+++ next.config.js
@@ -1,41 +1,18 @@
-// // next.config.js
-// module.exports = {
-//     async headers() {
-//       return [
-//         {
-//           // matching all API routes
-//           source: "/api/:path*",
-//           headers: [
-//             { key: "Access-Control-Allow-Credentials", value: "true" },
-//             { key: "Access-Control-Allow-Origin", value: "*" },
-//             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-//             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-//           ]
-//         }
-//       ]
-//     }
-//   };
+// next.config.js
+module.exports = {
+    async headers() {
+      return [
+        {
+          // matching all API routes
+          source: "/api/:path*",
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+          ]
+        }
+      ]
+    }
+  };
   
-const allowCors = fn =&gt; async (req, res) =&gt; {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
-
-const handler = (req, res) =&gt; {
-  const d = new Date()
-  res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
